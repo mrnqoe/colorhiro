21a188317b1e3a59ac852c98325da8c9394a8088
Scott Farmer <sfarmer664@gmail.com>
2017-02-25 16:22:51 +0000

    did nothing

--- colorhiro_vue/config/index.js
+++ colorhiro_vue/config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 4000,
+    port: 5000,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
