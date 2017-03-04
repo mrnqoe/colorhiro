19ae10f7cbf21105c12ac11e090cbfe5778aa8fb
Scott Farmer <sfarmer664@gmail.com>
2017-03-04 17:24:23 +0000

    threejs file

---
+++ colorhiro_vue/src/components/Threejs.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="room">
+    <h1>ROOM</h1>
+    <p>import</p>
+    <i  v-show="loading" class="fa fa-spinner fa-spin"></i>
+  </div>
+</template>
+
+
+<script>
+
+// import {post_data, get_data}        from '../helpers/queries.js'
+
+export default {
+
+}
+
+</script>
