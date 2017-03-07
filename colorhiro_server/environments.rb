91a5c4af21eb10060b5c73ee5694726c425bd986
Nicolas Moreau <mrnqoe@gmail.com>
2017-03-07 16:21:33 -0500

    fixing Sinatra DB call

--- colorhiro_server/environments.rb
+++ colorhiro_server/environments.rb
@@ -11,15 +11,6 @@ configure :test do
 end
 
 configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'db/colorhiro.sqlite3')
  set :server, :puma
  set :database, {adapter: "sqlite3", database: ENV['DATABASE_URL'] || 'db/colorhiro.sqlite3'}
- ActiveRecord::Base.establish_connection(
-   :adapter  => 'sqlite3',
-   :host     => ENV['DATABASE_HOST'],
-   :username => ENV['DATABASE_USER'],
-   :password => ENV['DATABASE_PASS'],
-   :database => ENV['DATABASE_DBURL'],
-   :encoding => 'utf8'
- )
 end
