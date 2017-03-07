41c7a62f9d2ddf6864457f8f4548fb43fdcf21d1
Nicolas Moreau <mrnqoe@gmail.com>
2017-03-07 15:52:54 -0500

    making the API

--- colorhiro_server/environments.rb
+++ colorhiro_server/environments.rb
@@ -12,13 +12,14 @@ end
 
 configure :production do
  db = URI.parse(ENV['DATABASE_URL'] || 'db/colorhiro.sqlite3')
-
+ set :server, :puma
+ set :database, {adapter: "sqlite3", database: ENV['DATABASE_URL'] || 'db/colorhiro.sqlite3'}
  ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'sqlite3' ? 'sqlite3' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
+   :adapter  => 'sqlite3',
+   :host     => ENV['DATABASE_HOST'],
+   :username => ENV['DATABASE_USER'],
+   :password => ENV['DATABASE_PASS'],
+   :database => ENV['DATABASE_DBURL'],
    :encoding => 'utf8'
  )
 end
